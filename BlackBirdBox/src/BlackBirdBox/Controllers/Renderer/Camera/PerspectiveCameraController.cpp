0e58b6c8a7814f9c4b491c00972a563d740912b6
Rok Kos <rok.kos@outfit7.com>
2020-09-12 14:50:03 +0200

    Fixed a bug where minimizing a window would crash whole app (aspect ratio was -inf -> Division by zero)

--- BlackBirdBox/src/BlackBirdBox/Controllers/Renderer/Camera/PerspectiveCameraController.cpp
+++ BlackBirdBox/src/BlackBirdBox/Controllers/Renderer/Camera/PerspectiveCameraController.cpp
@@ -1,111 +1,112 @@
-#include "bbbpch.h"
+﻿#include "bbbpch.h"
 #include "PerspectiveCameraController.h"
 
 #include "BlackBirdBox/Core/Input.h"
 
 namespace Core {
-	
 
-	PerspectiveCameraController::PerspectiveCameraController() :
-		camera_(16.0f / 9.0f, 45.0f, 0.01f, 1000.0f), prev_mouse_pos_(600.0f, 400.0f)
-	{
-		PROFILE_FUNCTION();
+PerspectiveCameraController::PerspectiveCameraController()
+    : camera_(16.0f / 9.0f, 45.0f, 0.01f, 1000.0f)
+    , prev_mouse_pos_(600.0f, 400.0f)
+{
+    PROFILE_FUNCTION();
 
-		SetCameraRotation();
-	}
+    SetCameraRotation();
+}
 
-	PerspectiveCameraController::PerspectiveCameraController(float aspect_ratio, float field_of_view, float near_clipping_plane, float far_clipping_plane) :
-		camera_(aspect_ratio, field_of_view, near_clipping_plane, far_clipping_plane), prev_mouse_pos_(600.0f, 400.0f)
-	{
-		PROFILE_FUNCTION();
+PerspectiveCameraController::PerspectiveCameraController(float aspect_ratio, float field_of_view, float near_clipping_plane, float far_clipping_plane)
+    : camera_(aspect_ratio, field_of_view, near_clipping_plane, far_clipping_plane)
+    , prev_mouse_pos_(600.0f, 400.0f)
+{
+    PROFILE_FUNCTION();
 
-		SetCameraRotation();
-	}
+    SetCameraRotation();
+}
 
-	void PerspectiveCameraController::OnUpdate(TimeStep ts)
-	{
-		PROFILE_FUNCTION();
+void PerspectiveCameraController::OnUpdate(TimeStep ts)
+{
+    PROFILE_FUNCTION();
 
-		glm::vec3 camera_pos = camera_.GetPosition();
+    glm::vec3 camera_pos = camera_.GetPosition();
 
-		if (Input::IsKeyPressed(KeyCode::A))
-		{
-			camera_pos -= glm::normalize(glm::cross(camera_.GetRotation(), camera_up_)) * camera_movement_speed_ * ts.GetSeconds();
-		}
-		else if (Input::IsKeyPressed(KeyCode::D)) {
-			camera_pos += glm::normalize(glm::cross(camera_.GetRotation(), camera_up_)) * camera_movement_speed_ * ts.GetSeconds();
-		}
+    if (Input::IsKeyPressed(KeyCode::A)) {
+        camera_pos -= glm::normalize(glm::cross(camera_.GetRotation(), camera_up_)) * camera_movement_speed_ * ts.GetSeconds();
+    } else if (Input::IsKeyPressed(KeyCode::D)) {
+        camera_pos += glm::normalize(glm::cross(camera_.GetRotation(), camera_up_)) * camera_movement_speed_ * ts.GetSeconds();
+    }
 
-		if (Input::IsKeyPressed(KeyCode::W))
-		{
-			camera_pos += camera_movement_speed_ * camera_.GetRotation() * ts.GetSeconds();
-		}
-		else if (Input::IsKeyPressed(KeyCode::S)) {
-			camera_pos -= camera_movement_speed_ * camera_.GetRotation() * ts.GetSeconds();
-		}
+    if (Input::IsKeyPressed(KeyCode::W)) {
+        camera_pos += camera_movement_speed_ * camera_.GetRotation() * ts.GetSeconds();
+    } else if (Input::IsKeyPressed(KeyCode::S)) {
+        camera_pos -= camera_movement_speed_ * camera_.GetRotation() * ts.GetSeconds();
+    }
 
-		camera_.SetPosition(camera_pos);
-	}
+    camera_.SetPosition(camera_pos);
+}
 
-	void PerspectiveCameraController::OnEvent(Event& e)
-	{
-		PROFILE_FUNCTION();
+void PerspectiveCameraController::OnEvent(Event& e)
+{
+    PROFILE_FUNCTION();
 
-		EventDispatcher dispatcher(e);
-		dispatcher.Dispatch<MouseMovedEvent>(BIND_EVENT_FN(PerspectiveCameraController::OnMouseMove));
-		dispatcher.Dispatch<MouseScrolledEvent>(BIND_EVENT_FN(PerspectiveCameraController::OnMouseScrolled));
-		dispatcher.Dispatch<WindowResizeEvent>(BIND_EVENT_FN(PerspectiveCameraController::OnWindowResized));
-	}
+    EventDispatcher dispatcher(e);
+    dispatcher.Dispatch<MouseMovedEvent>(BIND_EVENT_FN(PerspectiveCameraController::OnMouseMove));
+    dispatcher.Dispatch<MouseScrolledEvent>(BIND_EVENT_FN(PerspectiveCameraController::OnMouseScrolled));
+    dispatcher.Dispatch<WindowResizeEvent>(BIND_EVENT_FN(PerspectiveCameraController::OnWindowResized));
+}
 
-	bool PerspectiveCameraController::OnMouseMove(MouseMovedEvent& e)
-	{
-		PROFILE_FUNCTION();
+bool PerspectiveCameraController::OnMouseMove(MouseMovedEvent& e)
+{
+    PROFILE_FUNCTION();
 
-		const glm::vec2 mouse_pos = glm::vec2(e.GetX(), e.GetY());
+    const glm::vec2 mouse_pos = glm::vec2(e.GetX(), e.GetY());
 
-		if (Input::IsMouseButtonPressed(MouseCode::ButtonRight) || Input::IsMouseButtonPressed(MouseCode::ButtonMiddle)) {
-			glm::vec2 offset = mouse_pos - prev_mouse_pos_;
-			offset *= camera_rotation_speed_;
+    if (Input::IsMouseButtonPressed(MouseCode::ButtonRight) || Input::IsMouseButtonPressed(MouseCode::ButtonMiddle)) {
+        glm::vec2 offset = mouse_pos - prev_mouse_pos_;
+        offset *= camera_rotation_speed_;
 
-			yaw_ += offset.x;
-			pitch_ = glm::clamp(pitch_ - offset.y, -89.9f, 89.9f);
+        yaw_ += offset.x;
+        pitch_ = glm::clamp(pitch_ - offset.y, -89.9f, 89.9f);
 
-			SetCameraRotation();
-		}
+        SetCameraRotation();
+    }
 
-		prev_mouse_pos_ = mouse_pos;
+    prev_mouse_pos_ = mouse_pos;
 
-		return false;
-	}
+    return false;
+}
 
-	bool PerspectiveCameraController::OnMouseScrolled(MouseScrolledEvent& e)
-	{
-		PROFILE_FUNCTION();
+bool PerspectiveCameraController::OnMouseScrolled(MouseScrolledEvent& e)
+{
+    PROFILE_FUNCTION();
 
-		zoom_level_ -= e.GetYOffset() * 0.25f;
-		zoom_level_ = std::max(std::min(zoom_level_, 45.0f), 1.0f);
-		camera_.SetFieldOfView(zoom_level_);
-		return false;
-	}
+    zoom_level_ -= e.GetYOffset() * 0.25f;
+    zoom_level_ = std::max(std::min(zoom_level_, 45.0f), 1.0f);
+    camera_.SetFieldOfView(zoom_level_);
+    return false;
+}
 
-	bool PerspectiveCameraController::OnWindowResized(WindowResizeEvent& e)
-	{
-		PROFILE_FUNCTION();
+bool PerspectiveCameraController::OnWindowResized(WindowResizeEvent& e)
+{
+    PROFILE_FUNCTION();
 
-		const float aspect_ratio = (float)e.GetWidth() / (float)e.GetHeight();
-		camera_.SetAspectRatio(aspect_ratio);
-		return false;
-	}
+    if (e.GetHeight() == 0 || e.GetWidth() == 0) {
+        return false;
+    }
 
-	void PerspectiveCameraController::SetCameraRotation()
-	{
-		PROFILE_FUNCTION();
+    const float aspect_ratio = (float)e.GetWidth() / (float)e.GetHeight();
+    camera_.SetAspectRatio(aspect_ratio);
+    return false;
+}
 
-		glm::vec3 direction;
-		direction.x = cos(glm::radians(yaw_)) * cos(glm::radians(pitch_));
-		direction.y = sin(glm::radians(pitch_));
-		direction.z = sin(glm::radians(yaw_)) * cos(glm::radians(pitch_));
-		camera_.SetRotation(glm::normalize(direction));
-	}
+void PerspectiveCameraController::SetCameraRotation()
+{
+    PROFILE_FUNCTION();
+
+    glm::vec3 direction;
+    direction.x = cos(glm::radians(yaw_)) * cos(glm::radians(pitch_));
+    direction.y = sin(glm::radians(pitch_));
+    direction.z = sin(glm::radians(yaw_)) * cos(glm::radians(pitch_));
+    camera_.SetRotation(glm::normalize(direction));
+}
 
 }
--- BlackBirdBox/src/BlackBirdBox/Core/Application.cpp
+++ BlackBirdBox/src/BlackBirdBox/Core/Application.cpp
@@ -1,139 +1,136 @@
-#include "bbbpch.h"
+﻿#include "bbbpch.h"
 #include "Application.h"
 
-#include "Log.h"
-#include "Input.h"
-#include "TimeStep.h"
 #include "BlackBirdBox/Renderer/Renderer.h"
+#include "Input.h"
+#include "Log.h"
+#include "TimeStep.h"
 
 #include <glfw/glfw3.h>
 
 namespace Core {
 
-	Application* Application::s_Instance = nullptr;
+Application* Application::s_Instance = nullptr;
 
-	Application::Application()
-	{
-		PROFILE_FUNCTION();
+Application::Application()
+{
+    PROFILE_FUNCTION();
 
-		CORE_ASSERT(!s_Instance, "Application already exists!");
-		s_Instance = this;
-		m_Window = Window::Create();
-		m_Window->SetEventCallback(BIND_EVENT_FN(Application::OnEvent));
+    CORE_ASSERT(!s_Instance, "Application already exists!");
+    s_Instance = this;
+    m_Window = Window::Create();
+    m_Window->SetEventCallback(BIND_EVENT_FN(Application::OnEvent));
 
-		Renderer::Init();
+    Renderer::Init();
 
-		m_ImGuiLayer = new ImGuiLayer();
-		PushOverlay(m_ImGuiLayer);
-	}
+    m_ImGuiLayer = new ImGuiLayer();
+    PushOverlay(m_ImGuiLayer);
+}
 
-	Application::~Application()
-	{
-		PROFILE_FUNCTION();
+Application::~Application()
+{
+    PROFILE_FUNCTION();
 
-		Renderer::Shutdown();
-	}
+    Renderer::Shutdown();
+}
 
-	void Application::PushLayer(Layer* layer)
-	{
-		PROFILE_FUNCTION();
+void Application::PushLayer(Layer* layer)
+{
+    PROFILE_FUNCTION();
 
-		LOG_TRACE("Pushing Layer: {0}", layer->GetName());
-		m_LayerStack.PushLayer(layer);
-		layer->OnAttach();
-	}
+    LOG_TRACE("Pushing Layer: {0}", layer->GetName());
+    m_LayerStack.PushLayer(layer);
+    layer->OnAttach();
+}
 
-	void Application::PushOverlay(Layer* layer)
-	{
-		PROFILE_FUNCTION();
+void Application::PushOverlay(Layer* layer)
+{
+    PROFILE_FUNCTION();
 
-		LOG_TRACE("Pushing Overlay: {0}", layer->GetName());
-		m_LayerStack.PushOverlay(layer);
-		layer->OnAttach();
-	}
+    LOG_TRACE("Pushing Overlay: {0}", layer->GetName());
+    m_LayerStack.PushOverlay(layer);
+    layer->OnAttach();
+}
 
-	void Application::OnEvent(Event& e)
-	{
-		PROFILE_FUNCTION();
+void Application::OnEvent(Event& e)
+{
+    PROFILE_FUNCTION();
 
-		EventDispatcher dispatcher(e);
-		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(Application::OnWindowClose));
-		dispatcher.Dispatch<WindowResizeEvent>(BIND_EVENT_FN(Application::OnWindowResize));
-		dispatcher.Dispatch<KeyPressedEvent>(BIND_EVENT_FN(Application::OnKeyPressedEvent));
+    EventDispatcher dispatcher(e);
+    dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(Application::OnWindowClose));
+    dispatcher.Dispatch<WindowResizeEvent>(BIND_EVENT_FN(Application::OnWindowResize));
+    dispatcher.Dispatch<KeyPressedEvent>(BIND_EVENT_FN(Application::OnKeyPressedEvent));
 
-		for (auto it = m_LayerStack.rbegin(); it != m_LayerStack.rend(); ++it)
-		{
-			(*it)->OnEvent(e);
-			if (e.Handled)
-				break;
-		}
-	}
+    for (auto it = m_LayerStack.rbegin(); it != m_LayerStack.rend(); ++it) {
+        (*it)->OnEvent(e);
+        if (e.Handled)
+            break;
+    }
+}
 
-	void Application::Run()
-	{
+void Application::Run()
+{
 
-		PROFILE_FUNCTION();
+    PROFILE_FUNCTION();
 
-		while (m_Running)
-		{
-			float time = (float)glfwGetTime();
-			TimeStep timestep = time - m_LastFrameTime;
-			m_LastFrameTime = time;
+    while (m_Running) {
+        float time = (float)glfwGetTime();
+        TimeStep timestep = time - m_LastFrameTime;
+        m_LastFrameTime = time;
 
-			if (!m_Minimized)
-			{
-				{
-					for (Layer* layer : m_LayerStack)
-						layer->OnUpdate(timestep);
-				}
+        if (!m_Minimized) {
+            {
+                for (Layer* layer : m_LayerStack)
+                    layer->OnUpdate(timestep);
+            }
 
-				m_ImGuiLayer->Begin();
-				{
+            m_ImGuiLayer->Begin();
+            {
 
-					for (Layer* layer : m_LayerStack)
-						layer->OnImGuiRender();
-				}
-				m_ImGuiLayer->End();
-			}
+                for (Layer* layer : m_LayerStack)
+                    layer->OnImGuiRender();
+            }
+            m_ImGuiLayer->End();
+        }
 
-			m_Window->OnUpdate();
-		}
-	}
+        m_Window->OnUpdate();
+    }
+}
 
-	bool Application::OnWindowClose(WindowCloseEvent& e)
-	{
-		PROFILE_FUNCTION();
+bool Application::OnWindowClose(WindowCloseEvent& e)
+{
+    PROFILE_FUNCTION();
 
-		m_Running = false;
-		return true;
-	}
+    m_Running = false;
+    return true;
+}
 
-	bool Application::OnWindowResize(WindowResizeEvent& e)
-	{
+bool Application::OnWindowResize(WindowResizeEvent& e)
+{
 
-		PROFILE_FUNCTION();
+    PROFILE_FUNCTION();
 
-		if (e.GetWidth() == 0 || e.GetHeight() == 0)
-		{
-			m_Minimized = true;
-			return false;
-		}
+    if (e.GetWidth() == 0 || e.GetHeight() == 0) {
+        m_Minimized = true;
+        return false;
+    }
 
-		m_Minimized = false;
+    m_Minimized = false;
+    Renderer::OnWindowResize(e.GetWidth(), e.GetHeight());
 
-		return false;
-	}
+    return false;
+}
 
-	bool Application::OnKeyPressedEvent(KeyPressedEvent& e)
-	{
-		PROFILE_FUNCTION();
+bool Application::OnKeyPressedEvent(KeyPressedEvent& e)
+{
+    PROFILE_FUNCTION();
 
-		if (e.GetKeyCode() == KeyCode::Escape) {
-			m_Running = false;
-			return true;
-		}
+    if (e.GetKeyCode() == KeyCode::Escape) {
+        m_Running = false;
+        return true;
+    }
 
-		return false;
-	}
+    return false;
+}
 
 }
