3752771a551723ce2ddb06f3481ae9450e478d61
Rok Kos <rok.kos@outfit7.com>
2020-10-05 09:57:36 +0200

    Adding Audio Context to BlackBirdBox header

--- BlackBirdBox/src/BlackBirdBox.h
+++ BlackBirdBox/src/BlackBirdBox.h
@@ -1,12 +1,12 @@
-#pragma once
+﻿#pragma once
 
 // For use by Cloth Simulation
 
 #include "BlackBirdBox/Core/Core.h"
 
 #include "BlackBirdBox/Core/Application.h"
-#include "BlackBirdBox/Layer/Layer.h"
 #include "BlackBirdBox/Core/Log.h"
+#include "BlackBirdBox/Layer/Layer.h"
 
 #include "BlackBirdBox/Core/Timestep.h"
 
@@ -17,42 +17,44 @@
 #include "BlackBirdBox/ImGui/ImGuiLayer.h"
 
 // ---Renderer------------------------
-#include "BlackBirdBox/Renderer/Renderer.h"
-#include "BlackBirdBox/Renderer/RenderCommand.h"
-#include "BlackBirdBox/Renderer/Texture/Texture2D.h"
-#include "BlackBirdBox/Renderer/CubeMap/CubeMap.h"
-#include "BlackBirdBox/Renderer/Shader.h"
-#include "BlackBirdBox/Renderer/ShaderLibrary.h"
-#include "BlackBirdBox/Renderer/ModelLibrary.h"
 #include "BlackBirdBox/Renderer/Buffers/FrameBuffer.h"
 #include "BlackBirdBox/Renderer/Buffers/ShaderStorageArray.h"
+#include "BlackBirdBox/Renderer/CubeMap/CubeMap.h"
+#include "BlackBirdBox/Renderer/ModelLibrary.h"
+#include "BlackBirdBox/Renderer/RenderCommand.h"
+#include "BlackBirdBox/Renderer/Renderer.h"
+#include "BlackBirdBox/Renderer/Shader.h"
+#include "BlackBirdBox/Renderer/ShaderLibrary.h"
+#include "BlackBirdBox/Renderer/Texture/Texture2D.h"
 // -----------------------------------
-
 
 #include "BlackBirdBox/Components/Material.h"
 #include "BlackBirdBox/Primitives/PhongLightingParamaters.h"
-
 
 #include "BlackBirdBox/Events/KeyEvent.h"
 #include "BlackBirdBox/Events/MouseEvent.h"
 
 #include "BlackBirdBox/Controllers/Renderer/Camera/PerspectiveCameraController.h"
+#include "BlackBirdBox/ImGui/Menus/BaseMenu.h"
+#include "BlackBirdBox/ImGui/Menus/CameraMenu.h"
+#include "BlackBirdBox/ImGui/Menus/ComputeShaderMenu.h"
+#include "BlackBirdBox/ImGui/Menus/MiscMenu.h"
+#include "BlackBirdBox/ImGui/Menus/SceneViewMenu.h"
 #include "BlackBirdBox/Primitives/ComputeShaderConfiguration.h"
 #include "BlackBirdBox/Primitives/ComputeShaderSimulationConfiguration.h"
 #include "BlackBirdBox/Scene/Scene.h"
-#include "BlackBirdBox/ImGui/Menus/BaseMenu.h"
-#include "BlackBirdBox/ImGui/Menus/CameraMenu.h"
-#include "BlackBirdBox/ImGui/Menus/SceneViewMenu.h"
-#include "BlackBirdBox/ImGui/Menus/ComputeShaderMenu.h"
-#include "BlackBirdBox/ImGui/Menus/MiscMenu.h"
 
 #include "BlackBirdBox/Core/Util.h"
 #include "BlackBirdBox/JSON/JsonUtil.h"
 
-#include "BlackBirdBox/Tools/Instrumentator.h"
 #include "BlackBirdBox/Tools/InstrumentationTimer.h"
+#include "BlackBirdBox/Tools/Instrumentator.h"
 
-// Note(Rok Kos): Macros needs to be behind ImGui, because it includes Windows.h, look at this issue for further explanation: https://github.com/Tencent/rapidjson/issues/1448
+// Note(Rok Kos): Macros needs to be behind ImGui, because it includes Windows.h, look at this issue for further explanation:
+// https://github.com/Tencent/rapidjson/issues/1448
 #include "BlackBirdBox/Core/Macros.h"
 
 #include "BlackBirdBox/Physics/Cloth/Cloth.h"
+
+#include "BlackBirdBox/Audio/AudioContext.h"
+#include "BlackBirdBox/Audio/AudioSample.h"
